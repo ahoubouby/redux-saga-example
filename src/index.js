85f885672bbc1170dbd638e6e0f2b2638dc40f78
ahoubouby <ahoubouby@gmail.com>
2021-12-28 20:28:22 +0100

    config store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import server from './server';
+import configureStore from './store';
+
+const store = configureStore();
 
 server();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
